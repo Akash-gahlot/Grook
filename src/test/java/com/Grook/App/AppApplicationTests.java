e659ca20af04ba0946c066bc2502e913e2b632b3
Akash Gahlot <akashgahlot@Akashs-MacBook-Pro.local>
2025-01-21 13:52:13 +0530

    added yml part

--- src/test/java/com/Grook/App/AppApplicationTests.java
+++ src/test/java/com/Grook/App/AppApplicationTests.java
@@ -2,8 +2,13 @@ package com.Grook.App;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"spring.security.oauth2.client.registration.azure.client-id=test-client-id",
+		"spring.security.oauth2.client.registration.azure.client-secret=test-client-secret"
+})
 class AppApplicationTests {
 
 	@Test
